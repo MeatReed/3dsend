60d503bc68d62f8c090be9f4e2eff5a499b6eeba
MeatReed <tim.oumpho@gmail.com>
2020-05-24 16:28:21 +0200

    lint

--- src/renderer/pages/tinydb.vue
+++ src/renderer/pages/tinydb.vue
@@ -16,13 +16,21 @@
           color="primary"
         />
       </v-col>
-      <v-col v-if-else v-for="(item, index) of apps" :key="index" v-if="item.cia[0] !== undefined">
+      <v-col
+        v-if-else
+        v-for="(item, index) of apps"
+        :key="index"
+        v-if="item.cia[0] !== undefined"
+      >
         <v-card max-width="350" outlined>
           <v-card-title>
             {{ item.name }}
           </v-card-title>
           <div>
-            <v-card-subtitle>Auteur : {{ item.author }}<br />Version : {{ getVersion(item) }}</v-card-subtitle>
+            <v-card-subtitle
+              >Auteur : {{ item.author }}<br />Version :
+              {{ getVersion(item) }}</v-card-subtitle
+            >
           </div>
           <v-img
             class="white--text align-end"
@@ -50,20 +58,24 @@ export default {
     apps: null
   }),
   async fetch() {
-    const appsResponse = await this.$axios.$get('https://tinydb.eiphax.tech/api/apps')
+    const appsResponse = await this.$axios.$get(
+      'https://tinydb.eiphax.tech/api/apps'
+    )
     this.apps = appsResponse
   },
   methods: {
-    getQRCode: function (item) {
-      if(item.cia[0] === undefined) return;
-      return `https://tinydb.eiphax.tech/qr/${item.id}/${item.cia[item.cia.length - 1].id}/QR.png`
+    getQRCode: function(item) {
+      if (item.cia[0] === undefined) return
+      return `https://tinydb.eiphax.tech/qr/${item.id}/${
+        item.cia[item.cia.length - 1].id
+      }/QR.png`
     },
-    getVersion: function (item) {
-      if(item.cia[0] === undefined) return;
+    getVersion: function(item) {
+      if (item.cia[0] === undefined) return
       return item.cia[item.cia.length - 1].version
     },
-    download: function (item) {
-      if(item.cia[0] === undefined) return;
+    download: function(item) {
+      if (item.cia[0] === undefined) return
       remote.shell.openExternal(item.cia[item.cia.length - 1].download_url)
       return
     }
