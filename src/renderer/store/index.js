4a83129cfc1d351478f53cb3424d34441c7f72d9
MeatReed <tim.oumpho@gmail.com>
2020-05-27 03:04:47 +0200

    remove script in package.json 'lint fix'

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -2,8 +2,7 @@ import storage from 'electron-json-storage'
 
 export const actions = {
   generateURL({}, { file, ipV4, port }) {
-    const context = this
-    return context.$axios.$post(`http://${ipV4}:${port}/api/generateURL`, {
+    return this.$axios.$post(`http://${ipV4}:${port}/api/generateURL`, {
       file,
       port
     })
